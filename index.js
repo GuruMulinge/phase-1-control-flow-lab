807b7ae0c0442108dcfd2ee57dee1e66a5d85f8d
Anthony Mulinge <anthony.mulinge@student.moringaschool.com>
2023-03-15 19:08:14 +0300

    Assignment Commit

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  let greeting;
+  if (distanceInFeet <= 400) {
+    greeting = 'This one is on me!';
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (distanceInFeet > 2500) {
+    greeting = 'No can do.';
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(scuberGreetingForFeet(199)); 
+console.log(scuberGreetingForFeet(2000)); 
+console.log(scuberGreetingForFeet(2501)); 
+
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+console.log(ternaryCheckCity('NYC')); 
+console.log(ternaryCheckCity('LA'));
+
+
+function switchOnCharmFromTip(tipAmount) {
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+
+console.log(switchOnCharmFromTip('generous')); 
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('whatever')); 
